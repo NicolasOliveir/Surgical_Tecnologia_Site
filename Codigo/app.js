7685507b0b0da86ec6d2f4416ac7bc4172c27232
Nicolas Henrique <105471632+NicolasOliveir@users.noreply.github.com>
2023-09-11 13:31:00 -0300

    Update app.js

--- Codigo/app.js
+++ Codigo/app.js
@@ -8,7 +8,7 @@ function RedirecionaPolitica(){
 
 function SelecionaBarra(){
     var tela = window.location.pathname;
-    if (tela == '/index.html') {
+    if (tela == '/index.html'|| tela =='/') {
         $('.barra-de-navegacao').css("position", "absolute");
     }
     else {
@@ -33,10 +33,7 @@ SelecionaBarra();
 $('.sac').on('click',RedirecionaSAC);
 $('.politica').on('click',RedirecionaPolitica);
 
-var tela = window.location.pathname;
-if (tela == '/index.html'|| tela =='/') {
-    $('.barra-de-navegacao').css("position", "absolute");
-}
+
 
 $('#estado').on('change',function(){
     var option = this.selectedOptions[0];
